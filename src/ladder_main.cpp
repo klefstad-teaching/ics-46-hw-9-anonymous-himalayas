5b5dc688b1582e5e3561784da0b6ae602e2fc43a
Himal <himalm100@gmail.com>
2025-03-07 11:17:59 -0800

    added main function

---
+++ src/ladder_main.cpp
@@ -0,0 +1,17 @@
+#include "../src/ladder.h"
+
+
+void test_word_ladder()
+{
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    vector<string> ladder = generate_word_ladder("hit", "cog", word_list);
+    // verify_word_ladder();
+    print_word_ladder(ladder);
+}
+
+int main()
+{
+    test_word_ladder();
+    return 0;
+}
